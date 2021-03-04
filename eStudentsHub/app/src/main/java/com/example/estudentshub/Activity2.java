db62192ab125863849b787f2bd85abd748e00148
aanam0105 <aanam.mulla0105@gmail.com>
2021-03-04 12:41:55 +0530

    Add files via upload

---
+++ eStudentsHub/app/src/main/java/com/example/estudentshub/Activity2.java
@@ -0,0 +1,25 @@
+package com.example.estudentshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Activity2 extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        String linkUrl = getIntent().getStringExtra("link");
+        webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl(linkUrl);
+    }
+}
